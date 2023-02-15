da63ccc9f4eb2bf6e5f331f001bf1317ebeac542
mcyiyue <jngwiemalmsteen@gmail.com>
2023-02-15 17:39:18 +0800

    15/02/2023

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import {Transaction} from './Transaction'
+import axios from 'axios'
+import * as XLSX from 'xlsx'
 
 class App extends React.Component {
 
+
+  exportXl = () => {
+    axios.get('http://localhost:8080').then(function (response) {
+      const transaction=response.data
+      const formula=`SUM(L2:L${transaction.length+1})`;
+      const formula2=`SUM(Q2:Q${transaction.length+1})`;
+      const worksheet = XLSX.utils.json_to_sheet(transaction);
+      const workbook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(workbook, worksheet, "Transaction");
+      XLSX.utils.sheet_add_aoa(worksheet, [[
+        {t: "n", f: formula}]], { origin: `L${transaction.length+2}` });
+      XLSX.utils.sheet_add_aoa(worksheet, [[
+          {t: "n", f: formula2}]], { origin: `Q${transaction.length+2}` });
+      XLSX.writeFile(workbook, 'xlreact.xlsx')
+    }).catch(function (error) {
+      console.log(error)
+    })
+    
+  }
+
   render() { 
     return (
-      console.log(Transaction)
+      <button onClick={this.exportXl}></button>
     );
   }
 }
